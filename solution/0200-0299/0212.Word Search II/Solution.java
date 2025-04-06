e724d8234ea38664aaa430f5ba2b59d6d13b74d9
Mr. Khatri <117172329+361930@users.noreply.github.com>
2025-04-06 11:34:20 +0530

    Update Solution.java
    updating the solution with Java for 212.

--- solution/0200-0299/0212.Word Search II/Solution.java
+++ solution/0200-0299/0212.Word Search II/Solution.java
@@ -1,60 +1,55 @@
-class Trie {
-    Trie[] children = new Trie[26];
-    int ref = -1;
-
-    public void insert(String w, int ref) {
-        Trie node = this;
-        for (int i = 0; i < w.length(); ++i) {
-            int j = w.charAt(i) - 'a';
-            if (node.children[j] == null) {
-                node.children[j] = new Trie();
-            }
-            node = node.children[j];
-        }
-        node.ref = ref;
-    }
+class TrieNode {
+  public TrieNode[] children = new TrieNode[26];
+  public String word;
 }
 
 class Solution {
-    private char[][] board;
-    private String[] words;
-    private List<String> ans = new ArrayList<>();
+  public List<String> findWords(char[][] board, String[] words) {
+    for (final String word : words)
+      insert(word);
 
-    public List<String> findWords(char[][] board, String[] words) {
-        this.board = board;
-        this.words = words;
-        Trie tree = new Trie();
-        for (int i = 0; i < words.length; ++i) {
-            tree.insert(words[i], i);
-        }
-        int m = board.length, n = board[0].length;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                dfs(tree, i, j);
-            }
-        }
-        return ans;
+    List<String> ans = new ArrayList<>();
+
+    for (int i = 0; i < board.length; ++i)
+      for (int j = 0; j < board[0].length; ++j)
+        dfs(board, i, j, root, ans);
+
+    return ans;
+  }
+
+  private TrieNode root = new TrieNode();
+
+  private void insert(final String word) {
+    TrieNode node = root;
+    for (final char c : word.toCharArray()) {
+      final int i = c - 'a';
+      if (node.children[i] == null)
+        node.children[i] = new TrieNode();
+      node = node.children[i];
+    }
+    node.word = word;
+  }
+
+  private void dfs(char[][] board, int i, int j, TrieNode node, List<String> ans) {
+    if (i < 0 || i == board.length || j < 0 || j == board[0].length)
+      return;
+    if (board[i][j] == '*')
+      return;
+
+    final char c = board[i][j];
+    TrieNode child = node.children[c - 'a'];
+    if (child == null)
+      return;
+    if (child.word != null) {
+      ans.add(child.word);
+      child.word = null;
     }
 
-    private void dfs(Trie node, int i, int j) {
-        int idx = board[i][j] - 'a';
-        if (node.children[idx] == null) {
-            return;
-        }
-        node = node.children[idx];
-        if (node.ref != -1) {
-            ans.add(words[node.ref]);
-            node.ref = -1;
-        }
-        char c = board[i][j];
-        board[i][j] = '#';
-        int[] dirs = {-1, 0, 1, 0, -1};
-        for (int k = 0; k < 4; ++k) {
-            int x = i + dirs[k], y = j + dirs[k + 1];
-            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] != '#') {
-                dfs(node, x, y);
-            }
-        }
-        board[i][j] = c;
-    }
+    board[i][j] = '*';
+    dfs(board, i + 1, j, child, ans);
+    dfs(board, i - 1, j, child, ans);
+    dfs(board, i, j + 1, child, ans);
+    dfs(board, i, j - 1, child, ans);
+    board[i][j] = c;
+  }
 }
